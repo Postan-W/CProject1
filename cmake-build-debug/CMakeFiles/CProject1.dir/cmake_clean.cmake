171212dd32873da0dd55685ba98cdc3291d20d6d
15216 <15216802238@163.com>
2022-10-03 22:46:05 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/CProject1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProject1.dir/main.c.obj"
+  "CProject1.exe"
+  "CProject1.exe.manifest"
+  "CProject1.pdb"
+  "libCProject1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CProject1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
