3a06417fd6ba12a4690675a1bcd28e1f088db3d1
15216 <15216802238@163.com>
2022-10-03 23:05:55 +0800

    scanf(%d %d)

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int pay;
+
     const int PRICE = 100;
+    int pay;
     printf("价格是:%d\n",PRICE);
     printf("付款:");
     scanf("%d",&pay);
     printf("找零:");
-    printf("%d",pay - PRICE);
+    printf("%d\n",pay - PRICE);
+
+
+    printf("请输入两个整数:");
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("它们的和是:%d\n",a+b);
+
+
     return 0;
 }
